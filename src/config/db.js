6e2ea53e4a2f07e4e01c3593510191290cfc3ef7
Jawad-Jatri <jawad.jatri@gmail.com>
2024-12-09 00:16:54 +0600

    feat(refactor): test db seperated from main db, add coverage on test

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,5 @@
-const config = require('./index');
 const {Sequelize} = require('sequelize');
-const mysql = require('mysql2/promise');
-
-(async () => {
-    try {
-        // Connect to MySQL and create the database if it doesn't exist
-        const connection = await mysql.createConnection({
-            host: config.db.host,
-            user: config.db.username,
-            password: config.db.password,
-        });
-        await connection.query(`CREATE DATABASE IF NOT EXISTS ${config.db.database}`);
-        console.log(`Database "${config.db.database}" ensured.`);
-        await connection.end();
-    } catch (error) {
-        console.error('Error creating database:', error);
-    }
-})();
+const config = require('../config');
 
 const sequelize = new Sequelize(
     config.db.database,
@@ -28,11 +11,10 @@ const sequelize = new Sequelize(
     }
 );
 
-// Test the connection
 (async () => {
     try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
+        console.log('Connection to the database has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
