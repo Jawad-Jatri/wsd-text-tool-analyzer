72279f03b0330bb26c4b362e8f0bce5c3fafabd8
Jawad-Jatri <jawad.jatri@gmail.com>
2024-12-08 21:43:26 +0600

    feat(text model): text controller added

---
+++ src/controllers/textController.js
@@ -0,0 +1,30 @@
+const Text = require('../models/text');
+
+const textController = {
+    list: async (req, res) => {
+        res.render('index', {texts: await Text.findAll()});
+    },
+    create: async (req, res) => {
+        const {text} = req.body;
+        await Text.create({text});
+        res.redirect('/');
+    },
+    edit: async (req, res) => {
+        const {id} = req.params;
+        res.render('edit', {text: await Text.findOne(id)});
+    },
+    update: async (req, res) => {
+        const {id} = req.params;
+        const {text} = req.body;
+        await Text.update({text},
+            {where: {id: id}});
+        res.redirect('/');
+    },
+    delete: (req, res) => {
+        const {id} = req.params;
+        Text.destroy({where: {id: id}});
+        res.redirect('/');
+    },
+};
+
+module.exports = textController;
