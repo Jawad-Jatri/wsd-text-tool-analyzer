2fda7c976377537bc479033a49267c390ac5fe11
Jawad-Jatri <jawad.jatri@gmail.com>
2024-12-09 02:12:47 +0600

    feat(api): api controller added

---
+++ src/controllers/apiController.js
@@ -0,0 +1,16 @@
+const {Text} = require('../models');
+
+const apiController = {
+    wordCount: async (req, res) => {
+    },
+    characterCount: async (req, res) => {
+    },
+    sentenceCount: async (req, res) => {
+    },
+    paragraphCount: async (req, res) => {
+    },
+    longestWordsInParagraphs: (req, res) => {
+    },
+};
+
+module.exports = apiController;
