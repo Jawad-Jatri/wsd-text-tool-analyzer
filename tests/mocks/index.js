c5d33e2925545adafd0dad9a16ec5f769e4726a8
Jawad-Jatri <jawad.jatri@gmail.com>
2024-12-11 01:56:54 +0600

    feat(text-crud): unit test case added

---
+++ tests/mocks/index.js
@@ -0,0 +1,11 @@
+module.exports.fakeText = "The quick brown fox jumps over the lazy dog. \nThe lazy dog slept in the sun.";
+module.exports.fakeTexts = [
+    {
+        id: 1,
+        text: 'Hello World',
+    },
+    {
+        id: 2,
+        text: "The quick brown fox jumps over the lazy dog. \nThe lazy dog slept in the sun.",
+    }
+];
