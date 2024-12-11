07bb183747c61b1ab223b2f1f31b967b488238d6
Jawad-Jatri <jawad.jatri@gmail.com>
2024-12-12 01:27:05 +0600

    feat(oauth2): isAuthenticated middleware added for API

---
+++ src/middlewares/isAuthenticatedUser.js
@@ -0,0 +1,23 @@
+const UnauthorizedError = require('../common/exceptions/unauthorizedError');
+const ForbiddenError = require('../common/exceptions/forbiddenError');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const isAuthenticatedUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) return next(new UnauthorizedError('Not authorized'));
+
+        console.log('token', token);
+        jwt.verify(token, config.oauth.jwtToken, (err, user) => {
+            if (err) return next(new ForbiddenError('Forbidden'));
+            req.user = user;
+            next();
+        });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = isAuthenticatedUser;
