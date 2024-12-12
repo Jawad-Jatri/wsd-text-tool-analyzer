7897e2da383be5029630622896946683977b0c12
jawad <jawad.jatri@gmail.com>
2024-12-12 21:16:48 +0600

    feat(final-release): .env.example remove values, config setup refactor, config unit test case changes, README.md updated

--- src/config/index.js
+++ src/config/index.js
@@ -5,8 +5,8 @@ module.exports = {
     nodeEnv: process.env.NODE_ENV || 'development',
     db: {
         username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '@Kil201514057',
-        database: process.env.NODE_ENV === 'test' ? process.env.DB_NAME_TEST : process.env.DB_NAME || 'text_analyzer_db',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.NODE_ENV === 'test' ? process.env.DB_NAME_TEST : process.env.DB_NAME,
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 3306,
         dialect: process.env.DB_DIALECT || 'mysql',
